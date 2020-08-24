5fa3539ba1dcd0a0d17f4d62eb8a3c1074cbf0b2
Joseph-D98 <defelicejoseph@outlook.com>
2020-08-23 19:56:14 -0600

    countdown timer that stops at 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,163 +1,159 @@
-// HEADER 
-
-//Variables
-//VARIABLES
+//Variable IDs 
 var score = document.getElementById("score");
-var timer = document.getElementById("timer");
-
-//Text
-score.textContent = "View High Scores";
-timer.textContent = "Time: " + secondsLeft;
-
-//Style
-score.setAttribute("style", "color:purple; padding-left:20px;");
-timer.setAttribute("style", "float:right; padding-right:30px;");
-
-// Timer Function
-var secondsLeft = 80;
-var totalScore = 0;
-var secondsLeft = 90;  
-
-function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        finalScore();
-      }
-
-    }, 1000);
-  }
-
-// START QUIZ
-var h1 = document.querySelectorAll("h1");
-var text = document.querySelectorAll("p");
-var button = document.querySelector("button");
-h1[0].textContent = "Coding Quiz Challenge"
-text[0].textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your time by ten seconds";
-button.textContent = "Start Quiz";
-
-// Variables for questions 
 var start = document.getElementById("start");
-var explanation = document.getElementById("explanation");
 var submitButton = document.getElementById("submit");
+var explanation = document.getElementById("explanation");
+var submitButton = document.getElementById("submitButton");
 var quizQuestions = document.getElementById("quizQuestions");
 var choice1 = document.getElementById("1");
 var choice2 = document.getElementById("2");
 var choice3 = document.getElementById("3");
 var choice4 = document.getElementById("4");
-var correctAnswer = document.getElementById("correct");
-quizQuestions.setAttribute("style","font-size:20px; font-family:sans-serif;");
-choice1.setAttribute("style", "text-align:center; background-color:#8064A2; border-color:#8064A2; color:white;font-size:22px; padding: 0px 12px 0px 12px;");  
-choice2.setAttribute("style", "text-align:center; background-color:#8064A2; border-color:#8064A2; color:white;font-size:22px; padding: 0px 12px 0px 12px;");  
-choice3.setAttribute("style", "text-align:center; background-color:#8064A2; border-color:#8064A2; color:white;font-size:22px; padding: 0px 12px 0px 12px;");  
-choice4.setAttribute("style", "text-align:center; background-color:#8064A2; border-color:#8064A2; color:white;font-size:22px; padding: 0px 12px 0px 12px;");  
-correctAnswer.setAttribute("style","font-size:15px; font-family: sans-serif; color:grey font-style: italic");
 var answerResponse = document.getElementById("answerResponse");
 var allDone = document.getElementById("allDone")
-var finalScoreIs = document.getElementById("complete");
-var initials = document.getElementById("initials");
-var submitButton = document.getElementById("submit");
-var goBack = document.getElementById("goBack");
+var finalScoreIs = document.getElementById("finalScoreIs");
 var clearHighScores = document.getElementById("clearHighScores");
-
-//TEXT
-score.textContent = "View High Scores";
-timer.textContent = "Time: " + totalScore;
-start.textContent = "Coding Quiz Challenge"
+//Variable Classes 
+var quizChallengePage = document.querySelector(".quizChallengePage");
+var quizQuestionsPage = document.querySelector(".quizQuestionsPage");
+var finalScorePage = document.querySelector(".finalScorePage");
+//Coding Quiz Challenge Page 
+start.textContent = "Coding Quiz Challenge";
 explanation.textContent = "Try to answer the following code-related questions within 80 seconds. Every incorrect answers will penalize your time by ten seconds";
 submitButton.textContent = "Start Quiz";
+quizQuestionsPage.style.display = "none";// Hide Quiz Questions Page
+finalScorePage.style.display = "none"; // Hide Final Core Page 
+// START QUIZ / TIMER FUNCTION
+submitButton.addEventListener("click", startQuiz); // Start Quiz Button 
 
+  var secondsLeft = 80; // Seconds in Timer 
+  var secondsLeft = 3; // Seconds in Timer 
+  var startScore = 0; // Starting time 
+  var finalScore = "";  // Holder for Final Time 
+  var timer = document.getElementById("timer"); // Timer Variable 
 
-// QUESTIONS 
-var questions = [
-var quizQuestions = [
-    {
-        question: "Commonly used Data Types Include:",
-        choice1: "1. strings",
-@@ -95,38 +72,29 @@ var questions = [
-    }
-]
+timer.textContent = "Time: " + startScore; // Holder text in nav bar  
 
-//ALL DONE 
-var end = document.getElementById("allDone")
-var complete = document.getElementById("complete");
-var submitButton = document.getElementById("submit");
-var initials = document.getElementById("initials")
-end.setAttribute("style","font-size:20px; font-family: sans-serif; padding:20px;");
-finalScore.setAttribute("style","font-size:20px; font-family:sans-serif; padding:20px;");
-submitButton.setAttribute("style", "text-align:center; background-color:#8064A2; border-color:#8064A2; color:white;font-size:22px; padding: 0px 12px 0px 12px;");  
-initials.setAttribute("style","font-size:15px; font-family: sans-serif;");
-end.textContent = "All done!";
-complete.textContent = "Your final score is " + finalScore;
-submitButton.textContent - "Submit";
-initials.textContent - "Enter Initials";
+function startQuiz() { // Timer function Begins 
 
-//High Scores 
-
-
-
-
-
-
-
-  // Final Score Function 
-  function showScore() {
-    finalScore.textContent = " ";
-    mainEl.appendChild(imgEl);
-// timer.addEventListener("click", function();
-// Timer / Score Function 
-function submitButton() {
+  finalScorePage.style.display = "none"; // Hide Final Core Page 
+  quizChallengePage.style.display ="none"; // Hide Quiz Challenge Page 
+  quizQuestionsPage.style.display = "block"; // Show Quiz Questions Page
   var timerInterval = setInterval(function() {
     secondsLeft--;
+    timer.textContent = "Time: " + secondsLeft;
 
-  }
-  setTime();
-    if(secondsLeft === 0) {
-      var finalScore = secondsLeft - wrongAnswer; 
+    if (secondsLeft === 0 || quizQuestions.length === null) {
       clearInterval(timerInterval);
-      finalScorePage();
+
     }
   }, 1000);
 }
 
-  // Function that runs through question and answer 
-// Go to All Done page 
-function finalScorePage() {
-    allDone.textContent = "All done!";
-    finalScoreIs.textContent = "Your final score is " + finalScore;
-    submitButton.textContent - "Submit";
-    initials.textContent - "Enter Initials";
-  }
 
-// Function that runs through question and answer 
-var lastQuestionIndex = questions.length-1;
+// Questions Function  
+var lastQuestionIndex = quizQuestions.length-1;
 var questionUserAnswering = 0;
 
-@@ -145,6 +113,27 @@ function answerQuestion() {
-    renderQuestion ()
-}
+function answerQuestion() {
+    var q = questions[questionUserAnswering];
+    question.innerHTML ="<p>" + q.question+ "</p>";
+    choice1.innerHTML = q.choice1;
+    choice2.innerHTML = q.choice2;
+    choice3.innerHTML = q.choice3;
+    choice4.innerHTML = q.choice4;
 
- // Score Function NEED TO FINISH 
- //var finalScore = 
-// Final Score Function 
+    questionUserAnswering = 0; 
+    renderQuestion()
+
+    questionUserAnswering++
+    renderQuestion ()
+    // Questions Loop
+
+    for(var i=0; 1 < quizQuestions.length; i++) {
+    var answerResponse = document.createElement(quizQuestions[i].prompt)
+    if(answerResponse == quizQuestions[i].correct) {
+        startScore++;
+        answerResponse.textContent = "Correct!";
+    } else {
+        answerResponse.textContent = "Wrong!";
+    }
+  }
+};
+
+// Go to All Done page & Print Score
+function finalScorePage() {
+  quizChallengePage.style.display = "none"; // Hide Quiz Challenge Page
+  quizQuestionsPage.style.display = "none"; // Hide Questions Page
+  finalScorePage.style.display = "block"; // Show Final Score Page 
+    allDone.textContent = "All done!";
+    finalScoreIs.textContent = "Your final score is " + finalScore;
+  }
+//Function to add in initials - THINK IS DONE 
+var initials = document.getElementById("initials");
+var intitialInput = document.getElementById("initialInput")
+var initialButton = document.getElementById("initialButton");
+var submittedInitial = document.getElementById("response");
+initialButton.textContent = "Submit";
+initials.textContent = "Enter Initials";
+
+initialButton.addEventListener("click", function(event) { // click takes you to the high score page 
+  event.preventDefault();
+
+  console.log(event);
+   
+   var response = intitialInput.value;
+    submittedInitial.textContent = response; 
+ });
+ // Save score (use.value?)
+var highScore = localStorage.getItem("highScore");
+localStorage.setItem("highScore", highScore);
+// Print high scores 
 function showScore() {
   finalScore.textContent = " ";
   mainEl.appendChild(imgEl);
-
 }
-setTime();
-
- //Click Events 
-
- //View High Scores  
-
- //Start Quiz 
- start.addEventListener("click", start);
-
- //Quiz Answers
- //Submit 
-
- //Go Back Button 
- //Clear High Scores Button  
+ 
+// Questions
+var quizQuestions = [
+    {
+        question: "Commonly used Data Types Include:", 
+        choice1: "1. strings",
+        choice2: "2. booleans",
+        choice3: "3. alerts",
+        choice4: "4. numbers",
+        correct: "3",
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ________.",
+        choice1: "1. quotes",
+        choice2: "2. curly brackets",
+        choice3: "3. parenthesis",
+        choice4: "4. square brackets",
+        correct: "3",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ________.",
+        choice1: "1. numbers and strings",
+        choice2: "2. other arrays",
+        choice3: "3. booleans",
+        choice4: "4. all of the above",
+        correct: "4",
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables",
+        choice1: "1. commas",
+        choice2: "2. curly brackets",
+        choice3: "3. quotes",
+        choice4: "4. parenthesis",
+        correct: "3",
+    },
+    {
+        question: "A very useful tool used for developing and debugging for printing content to the debugger is:",
+        choice1: "1. JavaScript",
+        choice2: "2. terminal / bash",
+        choice3: "3. for loops",
+        choice4: "4. console.log",
+        correct: "4",
+    },
+];
